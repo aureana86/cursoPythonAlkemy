6ba5f3b1a68f412d133c46c11307227a8463da9a
aureana86 <aureanita2286@gmail.com>
2024-03-13 17:17:41 -0300

    search_fields working

--- Clase 9/miApp/nueva_app/admin.py
+++ Clase 9/miApp/nueva_app/admin.py
@@ -4,8 +4,7 @@ from . models import Tarea
 
 class TareaAdmin(admin.ModelAdmin):
     list_display = ["nombre", "completada", "responsable", "fecha_inicio", "fecha_termino"]
-"""     search_fields = ["nombre", "completada", "responsable",
-                     "fecha_inicio", "fecha_termino"]
- """
+    search_fields = ["nombre", "completada", "responsable", "fecha_inicio", "fecha_termino"]
+
 
 admin.site.register(Tarea, TareaAdmin)
